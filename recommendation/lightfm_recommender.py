2f4214f205a3845104118730ea3bc5696ca913c4
Diana-Cristina Urcan <urcan.diana10@yahoo.com>
2022-06-28 22:10:58 +0300

    updates on lightfm

--- recommendation/lightfm_recommender.py
+++ recommendation/lightfm_recommender.py
@@ -10,30 +10,11 @@ from song.models import Song
 
 class LightfmRecommender:
 
-    def create_features(dataframe, features_name, id_col_name):
-        features = dataframe[features_name].apply(
+    def get_data(dataset, info):
+        values = dataset[info].apply(
             lambda x: ','.join(x.map(str)), axis=1)
-        features = features.str.split(',')
-        features = list(zip(dataframe[id_col_name], features))
-        return features
-
-    def generate_feature_list(dataframe, features_name):
-        features = dataframe[features_name].apply(
-            lambda x: ','.join(x.map(str)), axis=1)
-        features = features.str.split(',')
-        features = features.apply(pd.Series).stack().reset_index(drop=True)
-        return features
-
-
-    def calculate_auc_score(lightfm_model, interactions_matrix, 
-                            question_features, professional_features): 
-        score = auc_score( 
-            lightfm_model, interactions_matrix, 
-            item_features=question_features, 
-            user_features=professional_features, 
-            num_threads=4).mean()
-        return score
-    
+        values = values.str.split(',')
+        return values
     
     qs = list(Likes.objects.all().values('song_id', 'user_int_id', 'liked'))
     df_users = pd.DataFrame(list(qs))
@@ -46,12 +27,11 @@ class LightfmRecommender:
     
     n_users = df_users['user_int_id'].unique().shape[0]
     n_items = df_songs['song_id'].unique().shape[0]
-    print("users=", n_users, "| items= ", n_items, "\n")
 
     song_weights = (df_users['song_id'].value_counts()) / df_users['song_id'].value_counts().max()
-    z1 = song_weights.to_dict()
+    song_data = song_weights.to_dict()
 
-    df_users['weight'] = df_users['song_id'].map(z1) 
+    df_users['weight'] = df_users['song_id'].map(song_data) 
     
     count = 0
     user_ids = []
@@ -78,11 +58,16 @@ class LightfmRecommender:
                 count += 1
     df_merged = df_users.merge(df_songs, how='inner', left_on='song_id', right_on='song_id')
     
-    songs_features_list = generate_feature_list(df_songs, ['tag', 'instrumentalness', 'valence', 'energy', 'danceability', 'acousticness', 'speechiness'])
-    users_features_list = generate_feature_list(df_users, ['user_int_id'])
+    song_info = get_data(df_songs, ['tag', 'instrumentalness', 'valence', 'energy', 'danceability', 'acousticness', 'speechiness'])
+    songs_features_list = song_info.apply(pd.Series).stack().reset_index(drop=True)
+    
+    user_info = get_data(df_users, ['user_int_id'])
+    users_features_list = user_info.apply(pd.Series).stack().reset_index(drop=True)
 
-    df_songs['song_features'] = create_features(df_songs, ['tag', 'instrumentalness', 'valence', 'energy', 'danceability', 'acousticness', 'speechiness'], 'song_id')
-    df_users['user_features'] = create_features(df_users, ['user_int_id'], 'user_int_id')
+    df_songs['song_features'] = list(zip(df_songs['song_id'], song_info))
+    df_users['user_features'] = list(zip(df_users['user_int_id'], user_info))
+    
+    df_merged['song_user_id_tuple'] = list(zip(df_merged['user_int_id'], df_merged['song_id'], df_merged['weight']))
     
     dataset = Dataset()
     dataset.fit(
@@ -91,7 +76,6 @@ class LightfmRecommender:
         item_features=songs_features_list, 
         user_features=users_features_list)
     
-    df_merged['song_user_id_tuple'] = list(zip(df_merged['user_int_id'], df_merged['song_id'], df_merged['weight']))
     interactions, weights = dataset.build_interactions(df_merged['song_user_id_tuple'])
             
     song_features = dataset.build_item_features(df_songs['song_features'])
@@ -115,38 +99,42 @@ class LightfmRecommender:
         num_threads=4, 
         verbose=True)
     
-    user_ids = [2]
-    for user in user_ids:
-        scores = model.predict(
-            user,
+    scores = model.predict(
+            2,
             df_songs['song_id'].values.tolist(),
             item_features=song_features,
             user_features=user_features)
         
-        v = (np.array(scores)).tolist()
-        df_2 = pd.DataFrame({'scores': v})
-        df_songs = df_songs.join(df_2)
-        df_songs = df_songs.sort_values(by='scores', ascending=False)[:8]
-        acc_train = calculate_auc_score(model, train_interactions, song_features, user_features)
-        acc_test = calculate_auc_score(model, test_interactions, song_features, user_features)
+    acc_train = auc_score( 
+            model, train_interactions, 
+            item_features=song_features, 
+            user_features=user_features, 
+            num_threads=4).mean()
+    
+    acc_test = auc_score( 
+            model, test_interactions, 
+            item_features=song_features, 
+            user_features=user_features, 
+            num_threads=4).mean()
          
-        precision_train = precision_at_k(model, train_interactions,
+    precision_train = precision_at_k(model, train_interactions,
                                user_features=user_features,
                                item_features=song_features,
                                num_threads=4).mean()
         
-        precision_test = precision_at_k(model, test_interactions,
+    precision_test = precision_at_k(model, test_interactions,
                                user_features=user_features,
                                item_features=song_features,
                                num_threads=4).mean()
         
-        recall_train = recall_at_k(model, train_interactions,
+    recall_train = recall_at_k(model, train_interactions,
                             user_features=user_features,
                             item_features=song_features).mean()
-        recall_test = recall_at_k(model, test_interactions,
+    recall_test = recall_at_k(model, test_interactions,
                             user_features=user_features,
                             item_features=song_features).mean()
-        print("\n\nPrecision is: train", precision_train, ", test: ", precision_test)
-        print("Recall is: train", recall_train, ", test: ", recall_test)
-        print("Accuracy is: train", acc_train, ", test: ", acc_test)
+    
+    print("\n\nPrecision is: train", precision_train, ", test: ", precision_test)
+    print("Recall is: train", recall_train, ", test: ", recall_test)
+    print("Accuracy is: train", acc_train, ", test: ", acc_test)
         
