793fd4a0e473f6e7296e6e36886dbcb7bcfb3e62
Diana-Cristina Urcan <urcan.diana10@yahoo.com>
2022-06-29 08:31:58 +0300

    updates

--- recommendation/views.py
+++ recommendation/views.py
@@ -14,7 +14,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 import random
 import tensorflow as tf
-from .lightfm_recommender import LightfmRecommender
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
